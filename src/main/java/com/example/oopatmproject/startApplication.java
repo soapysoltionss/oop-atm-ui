b18407915470e4ca4cd6401e183804d4807f0e32
Javier <superyoyo1000@gmail.com>
2023-03-03 11:39:01 +0800

    UI add enterPinNoPage.fxml and mainMenuPage and controller and rename pages

--- src/main/java/com/example/oopatmproject/startApplication.java
+++ src/main/java/com/example/oopatmproject/startApplication.java
@@ -4,19 +4,17 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class startApplication extends Application {
 
     Scene welcomePage, cardTerminalPage, enterPinNoPage, mainMenuPage, depositMoney1Page, depositMoney2Page, depositMoney3Page;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcomePage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(startApplication.class.getResource("enterPinNoPage.fxml"));
         Scene welcomePage = new Scene(fxmlLoader.load(),600,400);
         welcomePage.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
             @Override
--- src/main/java/com/example/oopatmproject/welcomeController.java
+++ src/main/java/com/example/oopatmproject/welcomeController.java
@@ -3,7 +3,7 @@ package com.example.oopatmproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class welcomeController {
     @FXML
     private Label welcomeText;
 
